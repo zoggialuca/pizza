3e53f0d4f3af48e5dfcbe502ce7a6e750c6abddb
zoggialuca <zoggialuca@gmail.com>
2021-07-29 15:21:43 +0200

    exception fix

--- src/main/java/com/pizza/pizza/exception/PizzaIngredientNotFoundException.java
+++ src/main/java/com/pizza/pizza/exception/PizzaIngredientNotFoundException.java
@@ -3,7 +3,7 @@ package com.pizza.pizza.exception;
 public class PizzaIngredientNotFoundException extends RuntimeException {
 
     public PizzaIngredientNotFoundException(Long id){
-        super(String.format("Pizza ingredient %i not found", id));
+        super(String.format("Pizza ingredient %d not found", id));
     }
     
 }
