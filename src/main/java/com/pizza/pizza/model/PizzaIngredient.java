551fd9ea7e43e97856c9ba748c12f137fba2db5c
zoggialuca <zoggialuca@gmail.com>
2021-08-12 09:50:39 +0200

    changes to run the application

--- src/main/java/com/pizza/pizza/model/PizzaIngredient.java
+++ src/main/java/com/pizza/pizza/model/PizzaIngredient.java
@@ -39,7 +39,7 @@ public class PizzaIngredient extends RepresentationModel<PizzaIngredient>{
     private UnitOfMeasure unitOfMeasure;
 
     protected PizzaIngredient(){
-        this(null, null);
+        this(new Pizza(), new Ingredient());
     }
 
     @Override
--- src/main/java/com/pizza/pizza/repository/PizzaIngredientRepository.java
+++ src/main/java/com/pizza/pizza/repository/PizzaIngredientRepository.java
@@ -13,11 +13,13 @@ public interface PizzaIngredientRepository  extends JpaRepository<PizzaIngredien
     List<PizzaIngredient> findByPizzaId(Long pizzaId);
     Optional<PizzaIngredient> findByPizzaIdAndIngredientId(Long pizzaId, Long ingredientId);
 
+    /*
     @Query(value = "insert into PizzaIngredient(pizza.id, ingredient.id, quantity, unitOfMeasure.id)" +
             "values(:pizzaId, :ingredientId, :quantity, :unitOfMeasureId)"
     )
     @Modifying
     PizzaIngredient insertJPQL(Long pizzaId, Long ingredientId, double quantity, Long unitOfMeasureId);
+    */
 
     @Query(value = "insert into pizza_ingredient(pizza_id, ingredient_id, quantity, unit_of_measure_id)" +
             "values(:pizzaId, :ingredientId, :quantity, :unitOfMeasureId)"
