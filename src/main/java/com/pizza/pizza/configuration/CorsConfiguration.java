08069cc7537092f0f3970ef906510ccdbe3abc39
zoggialuca <zoggialuca@gmail.com>
2021-08-12 13:21:33 +0200

    cross origin management fix

--- src/main/java/com/pizza/pizza/configuration/CorsConfiguration.java
+++ src/main/java/com/pizza/pizza/configuration/CorsConfiguration.java
@@ -13,7 +13,7 @@ public class CorsConfiguration {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/users").allowedOrigins(origins);
+                registry.addMapping("/**").allowedOrigins(origins);
             }
         };
     }
