398dc2b13d7b6fcbb267fd25f8719366f83a2f29
zoggialuca <zoggialuca@gmail.com>
2021-08-12 10:22:54 +0200

    HATEAOS link review for DTOs

--- src/main/java/com/pizza/pizza/dto/PizzaDTO.java
+++ src/main/java/com/pizza/pizza/dto/PizzaDTO.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.server.core.Relation;
 
 import javax.validation.constraints.NotBlank;
 
@@ -12,6 +13,7 @@ import javax.validation.constraints.NotBlank;
 @Setter
 @ToString
 @EqualsAndHashCode(callSuper = true)
+@Relation(collectionRelation = "pizzaList", itemRelation = "pizza")
 public class PizzaDTO extends RepresentationModel<PizzaDTO> {
     private Long id;
 
--- src/main/java/com/pizza/pizza/dto/PizzaIngredientResponseDTO.java
+++ src/main/java/com/pizza/pizza/dto/PizzaIngredientResponseDTO.java
@@ -1,5 +1,6 @@
 package com.pizza.pizza.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -17,6 +18,7 @@ public class PizzaIngredientResponseDTO extends RepresentationModel<PizzaIngredi
     private Long id;
 
     @NotNull
+    @JsonProperty("pizza")
     private PizzaDTO pizzaDTO;
 
     @NotNull
