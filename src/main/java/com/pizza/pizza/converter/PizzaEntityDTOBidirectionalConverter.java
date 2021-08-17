e04b04ab6d4445b3bbe360e9b47766d42928c2a4
Jakub Daze <jakub.daze@clusterreply.com>
2021-08-17 15:55:17 +0200

    fix for null isVegetarian

--- src/main/java/com/pizza/pizza/converter/PizzaEntityDTOBidirectionalConverter.java
+++ src/main/java/com/pizza/pizza/converter/PizzaEntityDTOBidirectionalConverter.java
@@ -10,23 +10,21 @@ import java.util.Optional;
 public class PizzaEntityDTOBidirectionalConverter implements EntityDTOBidirectionalConverter<Pizza, PizzaDTO> {
     @Override
     public PizzaDTO toDTO(Pizza pizza) {
-        if (pizza == null){
+        if (pizza == null) {
             return null;
         }
-
         var pizzaDTO = new PizzaDTO();
         pizzaDTO.setId(pizza.getId());
         pizzaDTO.setName(pizza.getName());
-        pizzaDTO.setVegetarian(pizza.getIsVegetarian());
+        pizzaDTO.setVegetarian(pizza.getIsVegetarian() != null && pizza.getIsVegetarian());
         return pizzaDTO;
     }
 
     @Override
     public Optional<Pizza> toOptionalEntity(PizzaDTO pizzaDTO) {
-        if (pizzaDTO == null){
+        if (pizzaDTO == null) {
             return Optional.empty();
         }
-
         var pizza = new Pizza(pizzaDTO.getName());
         pizza.setIsVegetarian(pizzaDTO.isVegetarian());
         pizza.setId(pizzaDTO.getId());
